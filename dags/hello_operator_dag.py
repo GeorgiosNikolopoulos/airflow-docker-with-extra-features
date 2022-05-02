5db5b1ba92befbf4295595ff3d7df26d169b3075
Georgios Nikolopoulos <nikolopoulosgwork@gmail.com>
2022-05-02 17:38:51 +0300

    Add Custom operator example

---
+++ dags/hello_operator_dag.py
@@ -0,0 +1,37 @@
+from custom_operator.hello_operator import HelloOperator
+from datetime import timedelta
+from airflow import DAG
+from airflow.utils.dates import days_ago
+# You can override them on a per-task basis during operator initialization
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+    # 'wait_for_downstream': False,
+    # 'dag': dag,
+    # 'sla': timedelta(hours=2),
+    # 'execution_timeout': timedelta(seconds=300),
+    # 'on_failure_callback': some_function,
+    # 'on_success_callback': some_other_function,
+    # 'on_retry_callback': another_function,
+    # 'sla_miss_callback': yet_another_function,
+    # 'trigger_rule': 'all_success'
+}
+
+with DAG(
+    'hello_operator_dag',
+    default_args=default_args,
+    description='Hello Dag',
+    schedule_interval=None,
+    start_date=days_ago(2),
+    tags=['custom'],
+) as dag:
+    hello_task = HelloOperator(task_id="sample-task", name="foo_bar")
